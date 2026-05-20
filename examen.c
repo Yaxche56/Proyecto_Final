881991c41d193a73990179098fc644b23bba9abc
Yaxché <yaxche65@gmail.com>
2026-05-19 23:14:46 -0600

    examen.c

--- examen.c
+++ examen.c
@@ -3,13 +3,11 @@
 #define MAX_OYENTES 100
 #define NUM_CANCIONES 10
 
-// --- PROTOTIPOS DE LAS FUNCIONES ---
 int leerVotos(int votos[][3]);
 void calcularVotosCanciones(int votos[][3], int num_oyentes, int puntaje_canciones[], int *primera, int *segunda);
 void determinarGanador(int votos[][3], int num_oyentes, int primera, int segunda);
 
 
-// --- FUNCIÓN PRINCIPAL ---
 int main() {
     int matriz_votos[MAX_OYENTES][3];
     int puntajes_canciones[NUM_CANCIONES];
@@ -18,16 +16,12 @@ int main() {
 
     printf("--- CONCURSO DE LA EMISORA DE RADIO ---\n\n");
 
-    // 1. Ejecutar Inciso 1: Leer y almacenar los datos
     total_oyentes = leerVotos(matriz_votos);
 
-    // Validar si al menos un oyente votó antes de procesar los resultados
     if (total_oyentes > 0) {
         
-        // 2. Ejecutar Inciso 2: Calcular estadísticas de canciones
         calcularVotosCanciones(matriz_votos, total_oyentes, puntajes_canciones, &primera_cancion, &segunda_cancion);
 
-        // 3. Ejecutar Inciso 3: Repartir puntos a los oyentes y elegir al ganador
         determinarGanador(matriz_votos, total_oyentes, primera_cancion, segunda_cancion);
 
     } else {
@@ -38,7 +32,6 @@ int main() {
 }
 
 
-// --- DESARROLLO DE LAS FUNCIONES ---
 
 // Inciso 1: Leer y almacenar los votos emitidos por cada oyente
 int leerVotos(int votos[][3]) {
@@ -52,12 +45,10 @@ int leerVotos(int votos[][3]) {
         printf("Oyente %d: ", num_oyentes);
         scanf("%d", &c1);
         
-        // Si el primer dato es -1, se detiene la captura de datos inmediatamente
         if (c1 == -1) {
             break;
         }
         
-        // Si no es -1, lee las otras dos opciones del trío
         scanf("%d %d", &c2, &c3);
 
         votos[num_oyentes][0] = c1;
@@ -66,7 +57,7 @@ int leerVotos(int votos[][3]) {
         num_oyentes++;
     }
     
-    return num_oyentes; // Retorna la cantidad total de oyentes que participaron
+    return num_oyentes; 
 }
 
 
@@ -76,27 +67,21 @@ void calcularVotosCanciones(int votos[][3], int num_oyentes, int puntaje_cancion
     int max1 = -1;
     int max2 = -1;
     
-    // Inicializar el arreglo de puntajes de canciones en 0
     for (i = 0; i < NUM_CANCIONES; i++) {
         puntaje_canciones[i] = 0;
     }
 
-    // Sumar puntos según la preferencia (1er lugar = 3pts, 2do lugar = 2pts, 3er lugar = 1pt)
     for (i = 0; i < num_oyentes; i++) {
         puntaje_canciones[votos[i][0]] += 3; 
         puntaje_canciones[votos[i][1]] += 2; 
         puntaje_canciones[votos[i][2]] += 1; 
     }
 
-    // Mostrar en pantalla la sumatoria de votos por canción
-    printf("\n=========================================\n");
     printf(" RESULTADOS DE LAS CANCIONES (INCISO 2) \n");
-    printf("=========================================\n");
     for (i = 0; i < NUM_CANCIONES; i++) {
         printf("Cancion %d: %d votos\n", i, puntaje_canciones[i]);
     }
 
-    // Buscar las dos canciones con los puntajes más altos (1ra y 2da)
     *primera = -1;
     *segunda = -1;
 
@@ -124,16 +109,13 @@ void determinarGanador(int votos[][3], int num_oyentes, int primera, int segunda
     int ganador = -1;
     int i, j;
 
-    printf("\n=========================================\n");
     printf("  PUNTAJES DE LOS OYENTES (INCISO 3)   \n");
-    printf("=========================================\n");
     
     for (i = 0; i < num_oyentes; i++) {
         int tiene_primera = 0;
         int tiene_segunda = 0;
         puntaje_oyentes[i] = 0;
 
-        // Validar si las canciones ganadoras se encuentran en las elecciones del oyente
         for (j = 0; j < 3; j++) {
             if (votos[i][j] == primera) {
                 tiene_primera = 1;
@@ -143,7 +125,6 @@ void determinarGanador(int votos[][3], int num_oyentes, int primera, int segunda
             }
         }
 
-        // Aplicar reglas de asignación de puntos
         if (tiene_primera) {
             puntaje_oyentes[i] += 30;
         }
@@ -156,14 +137,12 @@ void determinarGanador(int votos[][3], int num_oyentes, int primera, int segunda
 
         printf("Oyente %d: %d puntos\n", i, puntaje_oyentes[i]);
 
-        // Registrar cuál oyente va liderando el puntaje máximo
         if (puntaje_oyentes[i] > max_puntos) {
             max_puntos = puntaje_oyentes[i];
             ganador = i;
         }
     }
 
-    // Imprimir el veredicto final
     if (ganador != -1) {
         printf("\n¡GANADOR DEL CONCURSO!: El oyente numero %d con un total de %d puntos.\n", ganador, max_puntos);
     }
